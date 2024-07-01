6fafc8d1f44406b71ad30b886f17785d0bf289ad
Hieppppp <vuhiepk3a7@gmail.com>
2024-07-01 17:56:18 +0700

    update

--- app/Imports/CoursesImport.php
+++ app/Imports/CoursesImport.php
@@ -4,8 +4,10 @@ namespace App\Imports;
 
 use App\Models\Course;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class CoursesImport implements ToModel
+
+class CoursesImport implements ToModel, WithStartRow
 {
     /**
     * @param array $row
@@ -21,6 +23,10 @@ class CoursesImport implements ToModel
     }
     public function model(array $row)
     {
+        if (is_null($row[0])) {
+            return null;
+        }
+
         return new Course([
             'name' => $row[0],
             'description' => $row[1],
@@ -29,14 +35,20 @@ class CoursesImport implements ToModel
         ]);
     }
 
+    public function startRow(): int
+    {
+        return 2;
+    }
+
     public function rules(): array
     {
         return [
-            '0' => 'required|string|max:255',
-            '1' => 'required|string|max:5000',
-            '2' => 'required|date_format:Y-m-d',
-            '3' => 'required|date_format:Y-m-d|after_or_equal:2',
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:5000',
+            'start_date' => 'required|date_format:Y-m-d',
+            'end_date' => 'required|date_format:Y-m-d|after_or_equal:start_date',
         ];
+        
     }
 
     public function customValidationMessages()
